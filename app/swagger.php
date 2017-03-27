34dc71cb07a3c1fab1e8f71019da9e698884eee1
Mészáros Mihály <misi@niif.hu>
2017-03-27 09:16:25 +0200

    add swagger

---
+++ app/swagger.php
@@ -0,0 +1,13 @@
+<?php
+/**
+  * @SWG\Swagger(
+  *     schemes={"https"},
+  *     basePath="/v1",
+  *     @SWG\Info(
+  *         version="1.0",
+  *         title="TURN FRONTEND API",
+  *         description="TURN FrontEnd REST INTERFACE",
+  *         @SWG\Contact(name="STUN Ops Team", email="stun-devops@listserv.niif.hu")
+  *     ),
+  * )
+  */
