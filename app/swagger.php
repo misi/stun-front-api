76fe2904988e46f5dce232cc5816260f7e0a77e1
Mészáros Mihály <misi@niif.hu>
2017-03-27 09:39:20 +0200

    correction

--- app/swagger.php
+++ app/swagger.php
@@ -9,17 +9,20 @@
   *         description="TURN FrontEnd REST INTERFACE",
   *         @SWG\Contact(name="STUN Ops Team", email="stun-devops@listserv.niif.hu")
   *     ),
-  *     @SWG|SecurityScheme(
-  *         securityDefinition="clientauth",
-  *         description="OAuth Client Credenrials Grant Flow",
-  *         type="oauth2",
-  *         flow="application",
-  *         tokenUrl="https://vackor.lab.vvc.niif.hu/as/access_token",
-  *         scopes={
-  *             "ltc": "access to ltc service"
-  *             "rest": "access to rest"
-  *             "oauth": "access to oauth service"
-  *         },
-  *     ),
+  * )
+  */
+
+/**
+  * @SWG|SecurityScheme(
+  *     securityDefinition="clientauth",
+  *     description="OAuth Client Credenrials Grant Flow",
+  *     type="oauth2",
+  *     flow="application",
+  *     tokenUrl="https://vackor.lab.vvc.niif.hu/as/access_token",
+  *     scopes={
+  *         "ltc": "access to ltc service"
+  *         "rest": "access to rest"
+  *         "oauth": "access to oauth service"
+  *     },
   * )
   */
