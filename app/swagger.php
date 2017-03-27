d1b017f5ccfd8f6c2d8096f7458ae175fba49921
Mészáros Mihály <misi@niif.hu>
2017-03-27 10:04:17 +0200

    add ,

--- app/swagger.php
+++ app/swagger.php
@@ -13,16 +13,16 @@
   */
 
 /**
-  * @SWG|SecurityScheme(
+  * @SWG\SecurityScheme(
   *     securityDefinition="clientauth",
   *     description="OAuth Client Credenrials Grant Flow",
   *     type="oauth2",
   *     flow="application",
   *     tokenUrl="https://vackor.lab.vvc.niif.hu/as/access_token",
   *     scopes={
-  *         "ltc": "access to ltc service"
-  *         "rest": "access to rest"
-  *         "oauth": "access to oauth service"
+  *         "ltc": "access to ltc service",
+  *         "rest": "access to rest",
+  *         "oauth": "access to oauth service",
   *     }
   * )
   */
