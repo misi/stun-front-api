8cf3e2ef36fcfd338d4bb7fa950c52e74ce889ca
Mészáros Mihály <misi@niif.hu>
2017-05-17 21:36:56 +0200

    pdo drops exception.. so it isn't needed to handle errors in conventional way..

--- app/src/Repositories/ServerRepository.php
+++ app/src/Repositories/ServerRepository.php
@@ -28,42 +28,33 @@ class ServerRepository
      * @return ServerEntity[]
      */
     public function getServers(){
-      if($this->pdo->query("use ".$this->db)){
-          $sql="SELECT * FROM `server`";
-          if($server_stmt=$this->pdo->query($sql)){
-              $server_result=$server_stmt->fetchAll();
-              foreach ($server_result as $server_row) {
+        $this->pdo->query("use ".$this->db);
+        $sql="SELECT * FROM `server`";
+        $server_stmt=$this->pdo->query($sql);
+        $server_result=$server_stmt->fetchAll();
+        foreach ($server_result as $server_row) {
 
-                  $server = new ServerEntity();
+            $server = new ServerEntity();
 
-                  $server->fqdn = $server_row['fqdn'];
-                  $server->organization = $server_row['organization'];
-                  $server->latitude = $server_row['latitude'];
-                  $server->longitude = $server_row['longitude'];
+            $server->fqdn = $server_row['fqdn'];
+            $server->organization = $server_row['organization'];
+            $server->latitude = $server_row['latitude'];
+            $server->longitude = $server_row['longitude'];
 
-                  $sql="SELECT * FROM `ip` WHERE `server_id`=".$server_row['id'];
-                  if($ip_stmt=$this->pdo->query($sql)){
-                      $ip_result = $ip_stmt->fetchAll();
-                      foreach ($ip_result as $ip_row) {
-                          $ip = new IPEntity();
+            $sql="SELECT * FROM `ip` WHERE `server_id`=".$server_row['id'];
+            $ip_stmt=$this->pdo->query($sql);
+            $ip_result = $ip_stmt->fetchAll();
+            foreach ($ip_result as $ip_row) {
+                $ip = new IPEntity();
 
-                          $ip->address = $ip_row['address'];
-                          $ip->type = $ip_row['ipv6'] ? 'IPv6' : 'IPv4';
-                          $ip->preference = $ip_row['preference'];
+                $ip->address = $ip_row['address'];
+                $ip->type = $ip_row['ipv6'] ? 'IPv6' : 'IPv4';
+                $ip->preference = $ip_row['preference'];
 
-                          $server->ip[] = $ip;
-                      }
-                  }else{
-                      $this->logger->error('Error occurred during select from IP table!');
-                  }
-                  $servers[]=$server;
-              }
-              return $servers;
-          } else {
-              $this->logger->error('Error occurred during select from SERVER table!');
-          }
-      } else {
-        $this->logger->error('DB selection error occurred!');
-      }
+                $server->ip[] = $ip;
+            }
+            $servers[]=$server;
+        }
+        return $servers;
     }
 }
