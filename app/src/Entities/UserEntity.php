dfb1084e0714ab3079ecbb3e4e60250f34091646
Mészáros Mihály <misi@niif.hu>
2017-05-18 11:24:36 +0200

    Add eppn

--- app/src/Entities/UserEntity.php
+++ app/src/Entities/UserEntity.php
@@ -10,19 +10,29 @@ class UserEntity
      * @var string
      */
     public $username;
+
     /**
      * @SWG\Property(example="secret123")
      * @var string
      */
     public $password_clear;
+
     /**
      * @SWG\Property(example="68b329da9893e34099c7d8ad5cb9c940")
      * @var string
      */
     public $password_ha1;
+
     /**
      * @SWG\Property(example="turn.geant.org")
      * @var string
      */
     public $realm;
+
+    /**
+     * @SWG\Property(eppn="alice@atlanta.example")
+     * @var string
+     */
+    public $eppn;
+
 }
